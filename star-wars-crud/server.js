ede0b7b19ddd8334b3b47aa6376f0a4a900404f7
Brooke Kanonik <brookekanonik@gmail.com>
2023-09-09 14:55:54 -0400

    updating CRUD project

--- star-wars-crud/server.js
+++ star-wars-crud/server.js
@@ -12,14 +12,28 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database');
     const db = client.db('star-wars-quotes'); //does not matter the name of the db 
+    const quotesCollection = db.collection('quotes') //naming the collection quotes 
+    app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true})) //HAS to be before crud handlers
     app.get('/', (req,res) => { //instead of function (req,res){ } we can do an arrow function 
+        quotesCollection.find().toArray() //looking at the quotes collection and making it an array
+            .then(results => { //research more on promises and why this should be here 
+                console.log(results)
+            })
+            .catch(error => console.error(error))
         //res.send('Hello, World!') //send is writing hello world back to the browser 
-        res.sendFile(__dirname + '/index.html') //__dirname is directory name and where to grab the index.html file to serve it up
-    
+        //res.sendFile(__dirname + '/index.html') //__dirname is directory name and where to grab the index.html file to serve it up
+        res.render('index.ejs', {})
     })
     app.post('/quotes', (req, res) => {
-        console.log(req.body) //when you submit the form this should populate in the terminal 
+        quotesCollection.insertOne(req.body) //.body is because of bodyParser??
+            .then(result => { //.then is a promise 
+                console.log(result)
+                res.redirect('/') //going to redirect us home so we can make another entry if we want to/ stops the loading spinner from appearing.
+
+            })
+            .catch(error => console.error(error))
+        //console.log(req.body) //when you submit the form this should populate in the terminal 
     }) //grabbing the quotes path that was in the HTML
     app.listen(3000, function() { //app.listen is ensuring it is running on our 3000 port 
         console.log('listening on 3000')
