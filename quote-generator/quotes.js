b5f672e15742e78669e8ae6e2a835454c2b76c54
Brooke Kanonik <brookekanonik@gmail.com>
2023-09-10 12:47:52 -0400

    current code for quote generator

---
+++ quote-generator/quotes.js
@@ -0,0 +1,31 @@
+const localQuotes = [
+    {
+        text: 'Pompompurin is love. Pompompurin is life.',
+        author: 'Brooke',
+    },
+    {
+        text: 'Hello Kitty WHO?? Pompom time baby!',
+        author: 'Brooke (again)',
+    },
+    {
+        text: 'Cute yellow dogs ONLY (Pompompurin)',
+        author: 'B-B-Brooke',
+    },
+    {
+        text: 'This is a Pompompurin house. We love only Pompompurin here.',
+        author: 'Pompompurin',
+    },
+    {
+        text: 'Pompompurin was rated #2 most popular Sanrio character according to a Japan study conducted in 2022',
+        author: 'Brooke (and Japan facts via Parker)',
+    },
+    {
+        text: 'Need to go to Japan and buy all the Pompompurin merch I can!',
+        author: 'Brooke (I want to go to Japan)',
+    },
+    {
+        text: 'Thanks for testing the Pompompurin quote generator! Have a Pompom day!',
+        author: null,
+    },
+
+]
