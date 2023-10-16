2e05a15248d067c80fb576dce09e503f181064ca
Brooke Kanonik <brookekanonik@gmail.com>
2023-10-16 15:14:29 -0400

    forgot to save a file

--- socketio-chat-app/chat-room-frontend/src/components/MessageInput.js
+++ socketio-chat-app/chat-room-frontend/src/components/MessageInput.js
@@ -2,16 +2,33 @@ import React, {useState} from 'react'
 
 const MessageInput = ({sendMessage}) => {
     const [message, setMessage] = useState('')
-}
 
     // Function to handle form submission
     const handleSubmit = (e) => {
         // Prevent default form submission behavior
-     e.preventDefault();
+        e.preventDefault();
 
         // Call the sendMessage function passed as a prop to send the message
-     sendMessage(message);
+        sendMessage(message);
 
         // Clear the message input field
-     setMessage('');
+        setMessage('');
   };
+
+  // Render the message input form
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* Input field for chat message */}
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      {/* Submit button to send the message */}
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+// Exporting the MessageInput component for use in other parts of the application
+export default MessageInput;
