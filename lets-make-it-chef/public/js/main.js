5d68b49f2b8a0f961b512d4cc5e5f542c558b1a0
Brooke Kanonik <brookekanonik@gmail.com>
2023-10-23 16:41:56 -0400

    pushing to have reference to table data incase i want to go that route for adding recipes

--- lets-make-it-chef/public/js/main.js
+++ lets-make-it-chef/public/js/main.js
@@ -5,21 +5,32 @@ function add(){
   newField.setAttribute('type','text');
   newField.setAttribute('name','text');
   newField.setAttribute('class','text');
-  newField.setAttribute('siz',50);
+  newField.setAttribute('size',50);
   newField.setAttribute('placeholder','Ingredient');
 
   formfield.appendChild(newField);
-
-//   var newFieldAmount = document.createElement('input');
-//   newFieldAmount.setAttribute('type','text');
-//   newFieldAmount.setAttribute('name','text');
-//   newFieldAmount.setAttribute('class','text');
-//   newFieldAmount.setAttribute('siz',50);
-//   newFieldAmount.setAttribute('placeholder','Ingredient');
-//   formfield.appendChild(newFieldAmount);
 }
 
-function remove(){
-  var input_tags = formfield.getElementsByTagName('input');
+
+
+
+// let i = 0;
+
+// function add(i) {
+//   return `<tr data-index=${i}>
+//       <td>${i}</td>
+//       <td><input type="text" name="name-${i}"></td>
+//       <td><input type="text" name="phone-${i}"></td>
+//       <td><input type="text" name="Email-${i}"></td>
+//       <td><button onclick="removeRow(${i})">Remove <i class="fa fa-minus"></i></button></td>
+//     </tr>`
+// }
+
+// for (i = 0; i < 4; i ++) {
+//   $('tbody').append(rowTemplate(i));
+// }
+
+// function remove(){
+//   var input_tags = formfield.getElementsByTagName('input');
  
-  }
+//   }
