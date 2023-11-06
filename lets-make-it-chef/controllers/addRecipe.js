b1e8239ba0cb648b7483f6cc658015da866bf69b
Brooke Kanonik <brookekanonik@gmail.com>
2023-11-06 15:54:35 -0500

    adding updates to recipe website. as of now, just one ingredient and amount are able to be added but database is working. next step will be to correctly map the my recipe page to update naming when pulling data from recipes collection

--- lets-make-it-chef/controllers/addRecipe.js
+++ lets-make-it-chef/controllers/addRecipe.js
@@ -7,11 +7,14 @@ module.exports = {
 
     addRecipe: async (req, res)=>{
         try{
-            await Recipes.create({recipeInstructions: req.body.instructions, recipeIngredient: req.body.ingredient, recipeAmount: req.body.amount, userId: req.user.id}) //change Todo later
+            await Recipes.create({instructions: req.body.instructions, ingredient: req.body.ingredient, amount: req.body.amount, userId: req.user.id}) //change Todo later
             console.log('Recipe Has Been Added!')
             res.redirect('/my-recipes') //CREATE A YOUR RECIPE HAS BEEN CREATED SUCCESS MESSAGE
         }catch(err){
             console.log(err)
+            console.log(req.body)
+            //GETING CAUGHT HERE so it looks like it is not taking in the instructions, amount, etc check the add-recipes and compare. 
+           
         }
     },
 }
