dabc92b0ce9a8813612b2359e5e5fa0234ca9862
Brooke Kanonik <brookekanonik@gmail.com>
2024-01-05 19:47:45 -0500

    added additional code. need to fix toggling bug

--- task-maker/task-tracker/src/App.js
+++ task-maker/task-tracker/src/App.js
@@ -1,41 +1,86 @@
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "eat food",
-      day: "today",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "study",
-      day: "January 5th 2024",
-      reminder: true,
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-  ])
+ 
+    getTasks()
+  }, [])
+
+  //Fetch Tasks:
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+
+    return data
+  }
+
+  //Fetch Task:
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
   //Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = { id, ...task } //creating the new task
-  setTasks([...tasks, newTask]) //adding old tasks and new one to display
+const addTask = async (task) => {
+  const res = await fetch("http://localhost:5000/tasks", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json() //NEED TO AWAIT
+
+  setTasks([...tasks, data])
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = { id, ...task } //creating the new task
+  // setTasks([...tasks, newTask]) //adding old tasks and new one to display
 }
 
+
+
   //Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: "DELETE"
+  })
   setTasks(tasks.filter((task) => task.id !== id)) //do not show tasks with the id because we are deleting them
 }
 
 //Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder} : task))
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+
+    body: JSON.stringify(updTask),
+  })
+
+  const data = await res.json()
+
+  
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder : data.reminder} : task))
 }
 
  // && shorter way of doing a ternary without an else
