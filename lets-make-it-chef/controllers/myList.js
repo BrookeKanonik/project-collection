b49fa06e82656f8f2503621f6a4ff22178e2b81c
Brooke Kanonik <brookekanonik@gmail.com>
2024-01-22 11:04:13 -0500

    adjusting css and starting to add list components

--- lets-make-it-chef/controllers/myList.js
+++ lets-make-it-chef/controllers/myList.js
@@ -1,5 +1,16 @@
 module.exports = {
     getMyList: (req,res)=>{
         res.render('myList.ejs')
-    }
+    },
+
+    addItem: async (req, res)=>{
+        try{
+            console.log('Grocery Item Has Been Added!')
+            res.redirect('/list') 
+        }catch(err){
+            console.log(err)
+            console.log(req.body)
+            //GETING CAUGHT HERE so it looks like it is not taking in the instructions, amount, etc check the add-recipes and compare.           
+        }
+    },
 }
--- lets-make-it-chef/routes/myList.js
+++ lets-make-it-chef/routes/myList.js
@@ -6,6 +6,7 @@ const myListController = require('../controllers/myList')
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth,  myListController.getMyList) 
+router.post('/addItem', myListController.addItem)
 //router.post('/createRecipe', myRecipesController.createMyRecipes)
 //ENSURE AUTH IS CURRENTLY RETURNING TO THE HOMEPAGE WHEN USER LOGS IN, SEE WHY THIS IS, IS IT THEY ARENT ENSUREDAUTH AND IS EDFAULT DIRECTING THEM HOME??
 module.exports = router
