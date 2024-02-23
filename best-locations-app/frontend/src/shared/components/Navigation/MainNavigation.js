30fcfcd20d4612c7e200428aa2e25b9f8b707e8b
Brooke Kanonik <brookekanonik@gmail.com>
2024-02-23 16:41:59 -0500

    adding side drawer code before using props.show

--- best-locations-app/frontend/src/shared/components/Navigation/MainNavigation.js
+++ best-locations-app/frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {Link} from 'react-router-dom'
 // need to make the header clickable, so we are importing link from rrd
@@ -6,31 +6,46 @@ import {Link} from 'react-router-dom'
 import MainHeader from './MainHeader'
 import NavLinks from './NavLinks'
 import SideDrawer from './SideDrawer'
+import Backdrop from '../UIElements/Backdrop'
 import './MainNavigation.css'
 
 // inside the first button will be the hamburger menu 
+//drawerIsOpen && means if it is true, do the following else dont do any of what is inside 
 
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
+
     return (
     <React.Fragment>
-    <SideDrawer>
-        <nav className='main-navigation__drawer-nav'>
-            <NavLinks />
-        </nav>
-    </SideDrawer>
-    <MainHeader>
-        <button className='main-navigation__menu-btn'>
-            <span />
-            <span/>
-            <span/>
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to='/'>Your Places</Link>
-        </h1>
-        <nav className='main-navigation__header-nav'>
-            <NavLinks />
-        </nav>
-    </MainHeader>
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+        {drawerIsOpen && (<SideDrawer>
+            <nav className='main-navigation__drawer-nav'>
+                <NavLinks />
+            </nav>
+        </SideDrawer>
+        )}
+        <MainHeader>
+            <button className='main-navigation__menu-btn' onClick={openDrawer}>
+                <span />
+                <span/>
+                <span/>
+            </button>
+            <h1 className='main-navigation__title'>
+                <Link to='/'>Your Places</Link>
+            </h1>
+            <nav className='main-navigation__header-nav'>
+                <NavLinks />
+            </nav>
+        </MainHeader>
     </React.Fragment>
     )
 }
--- best-locations-app/frontend/src/shared/components/Navigation/SideDrawer.js
+++ best-locations-app/frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom"; //ReactDOM is the default export not {ReactDOM}
+import { CSSTransition } from 'react-transition-group'
 
 import './SideDrawer.css'
 
 const SideDrawer = props => {
-    return <aside className="side-drawer">{props.children}</aside>
+    const content =  (
+        <CSSTransition 
+            in={props.show} 
+            timeout={200} 
+            classNames="slide-in-left" 
+            mountOnEnter 
+            unmountOnExit
+        >
+            <aside className="side-drawer">{props.children}</aside>
+        </CSSTransition>
+    );
+
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
 };
 
+// going to create a portal and will display the content where drawer-hook is in the index.html
 export default SideDrawer;
