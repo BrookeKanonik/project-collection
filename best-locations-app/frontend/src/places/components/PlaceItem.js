1099334e5fd47d35536f53623a28df0b5ff39b8f
Brooke Kanonik <brookekanonik@gmail.com>
2024-03-01 19:16:28 -0500

    adding beginning of form information and also finished maps

--- best-locations-app/frontend/src/places/components/PlaceItem.js
+++ best-locations-app/frontend/src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import Modal from '../../shared/components/UIElements/Modal'
+import Map from '../../shared/components/UIElements/Map'
 import './PlaceItem.css'
 
 const PlaceItem = props => {
@@ -24,7 +25,7 @@ const PlaceItem = props => {
         >
             {/* what should be rendered in props children */}
             <div className="map-container">
-                <h2>MAP HERE</h2>
+                <Map center={props.coordinates} zoom={16}/>
             </div>
         </Modal>
     <li className="place-item">
--- best-locations-app/frontend/src/shared/components/UIElements/Map.js
+++ best-locations-app/frontend/src/shared/components/UIElements/Map.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 
 import './Map.css'
 
 const Map = props => {
-    return <div className={`map ${props.className}`} style={props.style}></div>
+    const mapRef = useRef(); //points to the div
+
+    const {center,zoom} = props; //so we can add as dependencies later on in the code
+
+    useEffect(() => {
+        const map = new window.google.maps.Map(mapRef.current, { //second param is where we configure the map
+
+            center: center, //center and zoom are dependencies, since we added center,zoom defined above, we can make props.zoom and props.center just center and zoom
+            zoom: zoom
+        });
+    
+        //below is how to render a marker
+        new window.google.maps.Marker({position: center, map: map})
+    }, [center,zoom]);
+
+
+
+    return (
+    <div 
+    ref={mapRef} 
+    className={`map ${props.className}`} 
+    style={props.style}>
+    </div>
+    )
 }
 
 export default Map;
