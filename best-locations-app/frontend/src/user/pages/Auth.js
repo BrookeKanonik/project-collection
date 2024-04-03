17544c92b14f0cbf8ca5fe2e36b28d112d398ef9
Brooke Kanonik <brookekanonik@gmail.com>
2024-04-02 20:55:33 -0400

    adding login auth

--- best-locations-app/frontend/src/user/pages/Auth.js
+++ best-locations-app/frontend/src/user/pages/Auth.js
@@ -8,7 +8,7 @@ import { useForm } from '../../shared/hooks/form-hook'
 import './Auth.css'
 
 const Auth = () => {
-    useForm({
+    const [formState, inputHandler] = useForm({
         email: {
             value: '',
             isValid: false
@@ -18,11 +18,34 @@ const Auth = () => {
             isValid: false
         }
     }, false);
+
+    const authSubmitHandler = event => {
+        event.preventDefault(); //to prevent auto submission of request and reload of page
+        console.log(formState.inputs);
+    }
     return <Card className = 'authentication'> 
         <h2>Login Required</h2>
         <hr/>
-        <form>
-            <Input element="input" id="email" type="email" label="E-Mail" validators={[VALIDATOR_EMAIL()]} errorText= "please enter a valid email" />
+        <form onSubmit={authSubmitHandler}>
+            <Input 
+                element="input" 
+                id="email" 
+                type="email" 
+                label="E-Mail" 
+                validators={[VALIDATOR_EMAIL()]} 
+                errorText= "please enter a valid email" 
+                onInput= {inputHandler}
+            />
+            <Input 
+                element="input" 
+                id="password" 
+                type="password" 
+                label="Password" 
+                validators={[VALIDATOR_MINLENGTH(5)]} 
+                errorText= "please enter a valid password, at least 5 characters" 
+                onInput= {inputHandler}
+            />
+            <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
         </form>
         </Card>
 }
