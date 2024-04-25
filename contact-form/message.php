dd317181f159271e0f468133794f1bcf8b52e586
Brooke Kanonik <brookekanonik@gmail.com>
2024-04-25 19:36:58 -0400

    final form prior to server

--- contact-form/message.php
+++ contact-form/message.php
@@ -1,12 +1,25 @@
 <?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $website = $_POST['website'];
-    $message = $_POST['message'];
+    $name = filter_input(INPUT_POST, 'name');
+    $email = filter_input(INPUT_POST, 'email');
+    $phone = filter_input(INPUT_POST, 'phone');
+    $website = filter_input(INPUT_POST, 'website');
+    $message = filter_input(INPUT_POST, 'message');
+
 
     if(!empty($email) && !empty($message)){
-
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver = "brookekanonik@gmail.com";
+            $subject = "From: $name <$email>" ;//subject of the email
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards, \n$name";
+            $sender = "From: $email";
+            if(mail($receiver, $subject, $body, $sender)){
+                echo 'Your message has been sent!';
+            }else{
+                echo 'Sorry, failed to send your message!';
+            }
+        }else {
+            echo 'Enter a valid email address!';
+        }
     }else{
         echo 'Email and password field is required!';
     }
--- contact-form/script.js
+++ contact-form/script.js
@@ -3,14 +3,26 @@ statusTxt = form.querySelector(".button-area span")
 
 form.onsubmit = (e) => { //when we click the submit button...
      e.preventDefault(); //prevent form from submitting 
+     statusTxt.style.color = "grey";
      statusTxt.style.display ="block"; //once the button is submitted show the message
+     statusTxt.innerHTML = "Sending your message...";
 
      let xhr = new XMLHttpRequest(); //create new xml object
      xhr.open("POST", "message.php", true); //sending post request to message.php file 
      xhr.onload = () => { //once ajax loaded
         if (xhr.readyState == 4 && xhr.status == 200){ //if ajax response status is 200 and ready status is 4 that means there is no error 
             let response = xhr.response;
-            console.log(response);
+            //for errors we will change the color to be red
+            if (response.indexOf('Email and password field is required!') != -1 || response.indexOf('Enter a valid email address!') || response.indexOf('Sorry, failed to send your message!')){
+                statusTxt.style.color = "red";
+            }else{
+                form.reset();
+                setTimeout(() => {
+                    statusTxt.style.display ="none";
+                }, 3000)
+            }
+            //console.log(response); //response can also link to the message.php in our if statements
+            statusTxt.innerHTML = response;
         }
      }
      let formData = new FormData(form); //creating a new FormData object that is used to send form data
