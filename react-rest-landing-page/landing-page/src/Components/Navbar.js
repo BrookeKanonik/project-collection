25c3d089e88827bd12c8a8e4ddb88f8290d9c117
Brooke Kanonik <brookekanonik@gmail.com>
2024-05-20 15:06:25 -0400

    adding react rest landing page

---
+++ react-rest-landing-page/landing-page/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from "../"
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
+//get assets from github repo
