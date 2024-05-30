5dfcd648721623f27f0bf8815db656f1ac5a75f3
Brooke Kanonik <brookekanonik@gmail.com>
2024-05-30 15:25:55 -0400

    adding ramen restaurant page

---
+++ ramen-restaurant-page/landing-page/src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutBackground from '../Assets/about-background.png'
+import AboutBackgroundImage from '../Assets'
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
