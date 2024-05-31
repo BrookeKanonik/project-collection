e4fecd66d3af7d29b8ca8fbd0d2d293a0ac0a7f5
Brooke Kanonik <brookekanonik@gmail.com>
2024-05-30 22:53:19 -0400

    adding work component

--- ramen-restaurant-page/landing-page/src/Components/Work.js
+++ ramen-restaurant-page/landing-page/src/Components/Work.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PickMeals from '..Assets/pick-meals=image.png'
+import PickMeals from '../Assets/pick-meals-image.png'
 import ChooseMeals from '../Assets/choose-image.png'
 import DeliveryMeals from '../Assets/delivery-image.png'
 
@@ -13,17 +13,33 @@ const Work = () => {
         {
             image: ChooseMeals,
             title: "Choose Extras",
-            text: 'Choose one (or more) of our many selections of Ramen. We highly recommend eating it fresh from our shop, but we won\'t stop you if you want it delivered!'
+            text: 'Maybe you\'re feeling fancy? Why not add an egg and more bamboo shoots. Check which extras you can add on the item page.'
         },
         {
             image: DeliveryMeals,
-            title: "We Deliver",
-            text: 'Choose one (or more) of our many selections of Ramen. We highly recommend eating it fresh from our shop, but we won\'t stop you if you want it delivered!'
+            title: "Fast Deliveries",
+            text: 'If you are within a 3 mile radius, our team will personally deliver to you.'
         },
     ]
   return (
-    <div>
-      Work
+    <div className='work-section-wrapper'>
+      <div className='work-section-top'>
+        <p className='primary-subheading'>Work</p>
+        <h1 className='primary-heading'>How It Works</h1>
+      </div>
+      <div className='work-section-bottom'>
+        {
+            workInfoData.map((data) => (
+                <div className='work-section-info'>
+                    <div className='info-boxes-img-container'>
+                        <img src={data.image} alt="" />
+                    </div>
+                    <h2>{data.title}</h2>
+                    <p>{data.text}</p>
+                </div>
+            ))
+        }
+      </div>
     </div>
   )
 }
